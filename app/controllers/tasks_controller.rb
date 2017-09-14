52ed9bdc22bd7fe39737a9a845ce74fef8baf893
ga.hos <gagigigiga@gmail.com>
2017-09-14 14:07:17 +0000

    only correct user read

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in,only: [:edit, :update, :destroy]
-  before_action :require_correct_user,only: [:edit, :update, :destroy]
+  before_action :require_correct_user,only: [:show, :edit, :update, :destroy]
   before_action :set_task,only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all.page(params[:page])
+    if logged_in?
+      @tasks = current_user.tasks.all.page(params[:page])
+    end
   end
 
   def show
